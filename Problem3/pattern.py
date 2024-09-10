9792f98011e2c05bb7a3f2d63c60d8214d902354
Keshavshetty707 <mkeshav9241217275@gmail.com>
2024-09-10 21:03:47 +0530

    problem three code

---
+++ Problem3/pattern.py
@@ -0,0 +1,8 @@
+numb = int(input("Enter a number: "))
+
+if numb %2 == 1: #input 5
+    # print(numb%2,"if")  output 1 if
+    while numb < 1:
+         
+else:
+    # print(numb%2,"else") output  else
