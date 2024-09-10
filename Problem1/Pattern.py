ebd9f91f5ef8a851365244d31e7654253430c523
Keshavshetty707 <mkeshav9241217275@gmail.com>
2024-09-10 21:00:23 +0530

    problem one code

---
+++ Problem1/Pattern.py
@@ -0,0 +1,12 @@
+# def pattern(n):
+#     for i in range(n, 0, -1):
+#         for j in range(i, 6):
+#             print(j, end="")
+#         while(i<0):
+#             print(n, end="")
+#             i-=1
+#         print()
+
+# rows = int(input("Enter number of rows: "))
+# pattern(rows)
+
