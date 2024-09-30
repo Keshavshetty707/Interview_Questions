22bb47ede087f4cf173ca1ed1ae46c9bc686d6d8
Keshavshetty707 <mkeshav9241217275@gmail.com>
2024-09-30 21:35:46 +0530

    problem solved and we can get output in at runtime

--- Problem1/Pattern.py
+++ Problem1/Pattern.py
@@ -1,12 +1,21 @@
-# def pattern(n):
-#     for i in range(n, 0, -1):
-#         for j in range(i, 6):
-#             print(j, end="")
-#         while(i<0):
-#             print(n, end="")
-#             i-=1
-#         print()
+#this problem take so much time so i didn't touched it for long time, on day 9/30/2024 I randomly started coding finaly i got a solution
+#I writtern this program dinamically
+def print_pattern(n):
+    edit_pattern=0
+    for i in range(n, 0, -1):    
+        for j in range(i, n + 1):
+            print(j, end="")
+        j=i
+        while j-1 > 0:
+            print(i+edit_pattern,end="")
+            j-=1
+        print()
+        edit_pattern=edit_pattern+1
 
-# rows = int(input("Enter number of rows: "))
-# pattern(rows)
+n = int(input("Enter number of rows: "))
+print_pattern(n)
+
+
+
+
 
