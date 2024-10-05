e060e0ca09b0900adf7c59b830ae9ddc01c829e5
Keshavshetty707 <mkeshav9241217275@gmail.com>
2024-10-05 10:07:50 +0530

    removed a comment

--- Problem1/Pattern.py
+++ Problem1/Pattern.py
@@ -1,5 +1,4 @@
-#this problem take so much time so i didn't touched it for long time, on day 9/30/2024 I randomly started coding finaly i got a solution
-#I writtern this program dinamically
+
 def print_pattern(n):
     edit_pattern=0
     for i in range(n, 0, -1):    
